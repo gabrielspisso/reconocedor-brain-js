7a283bf14a1d4c72a2d90a7493369ef3c3768184
gabrielspisso <gabrielhspisso@gmail.com>
2019-04-19 19:04:18 -0300

    Subo ejemplo

---
+++ index.js
@@ -0,0 +1,51 @@
+var brain = require('brain.js')
+// provide optional config object (or undefined). Defaults shown.
+const config = {
+    binaryThresh: 0.5,
+    hiddenLayers: [3],     // array of ints for the sizes of the hidden layers in the network
+    activation: 'sigmoid',  // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
+    leakyReluAlpha: 0.01   // supported for activation type 'leaky-relu'
+};
+
+// create a simple feed forward neural network with backpropagation
+const net = new brain.NeuralNetwork(config);
+const trainingData = [
+    {
+        input: "Pollo",
+        output: { asistencia: 0.5, dificultad: 2}
+    },
+    {
+        input: "Albione",
+        output: { gay: 1 }
+    }
+];
+
+
+let trainedNet;
+
+function encode(arg) {
+    return arg.split('').map(x => (x.charCodeAt(0) / 256));
+}
+
+function processTrainingData(data) {
+    return data.map(d => {
+        return {
+            input: encode(d.input),
+            output: d.output
+        }
+    })
+}
+
+function train(data) {
+    let net = new brain.NeuralNetwork();
+    net.train(processTrainingData(data));
+    trainedNet = net.toFunction();
+};
+
+function execute(input) {
+    let results = trainedNet(encode(input));
+    console.log(results)
+}
+
+train(trainingData);
+execute("Pollo");
