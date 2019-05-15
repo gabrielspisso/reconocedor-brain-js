303969dcd768d5ffb3f39b315bb3267eae49fe9f
dperezferrando <diego@parsimotion.com>
2019-05-15 19:22:36 -0300

    Controller

---
+++ controller.js
@@ -0,0 +1,22 @@
+class Controller {
+
+  verArtistas(req, res) {
+    
+
+  }
+
+  agregarArtista(req, res) {
+
+  }
+
+  entrenarRed(req, res) {
+
+  }
+
+  buscarArtista(req, res) {
+
+  }
+
+}
+
+module.exports = new Controller();
--- routes.js
+++ routes.js
@@ -1,36 +1,12 @@
-var express = require('express');
-var app = express();
+const app = require('express')();
+const controller = require("./controller");
+const PORT = process.env.PORT || 9001;
+
+app.get('/', controller.verArtistas);
+app.get('/buscarArtista', controller.buscarArtista);
+app.post('/agregar', controller.agregarArtista);
+app.post('/entrenarRed', controller.entrenarRed)
 
 
-const port=process.env.PORT || 3000;
-let redEntrenada;
-
-app.get('/', function (req, res) {
-
-  res.send(handler.verArtistas);
-});
-
-
-//Para probar usar por ejemplo: http://localhost:3000/agregar?artista=la%20renga
-app.get('/agregar', function (req, res) {//convertirlo en post
-  var artista = req.query.artista; 
-  handler.agregarArtista(artista);
-  res.send("puto el que lee");
-});
-
-
-app.get('/entrenarRed', function (req, res) {
-  entrenarRed();
-  res.send("La red ha sido entrenada");
-});
-
-app.get('/buscarArtista', function (req, res) {//convertirlo en post
-  var input = req.query.input; 
-  res.send(ejecutar(input));
-});
-
-
-app.listen(port, function () {
-  console.log(`Server running at port `+port);
-});
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 
