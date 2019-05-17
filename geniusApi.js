a22fa729eb1f043e4b291c7bc2d56bbe64221a20
dperezferrando <diego@parsimotion.com>
2019-05-17 00:06:10 -0300

    Pido 50

--- geniusApi.js
+++ geniusApi.js
@@ -18,7 +18,7 @@ class GeniusApi {
   }
 
   cancionesDelArtista(idArtista) {
-    return Promise.resolve(this.api.songsByArtist(idArtista, { per_page: 5, sort: 'popularity'}))
+    return Promise.resolve(this.api.songsByArtist(idArtista, { per_page: 50, sort: 'popularity'}))
       .get("songs")
       .map(({ id }) => lyricist.song(id, { fetchLyrics: true }))
       .map(({ id, title: titulo, lyrics: letra, primary_artist: { id: idArtista, name: artista } }) => ({ id, titulo, letra, idArtista, artista }))
